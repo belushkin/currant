1eed1252a6ccb4a295a2d76fab6c322a2e0d4123
Daniil Boiko <danylob@getblend.com>
2021-10-22 13:19:43 +0300

    Make player behavior only for self

--- code/main.js
+++ code/main.js
@@ -39,7 +39,7 @@ k.scene("battle", () => {
   // Mission
   showMission();
 
-  const player = getPlayer("currant");
+  const player = getPlayer("currant", true);
   playerModel = new PlayerModel(name, width() / 2, height() / 2, player);
   mp = new Multiplayer(playerModel);
 
--- code/src/player.js
+++ code/src/player.js
@@ -4,7 +4,7 @@ import addExplode from "./explode";
 
 loadSprite("bean", "sprites/bean.png");
 
-export default function getPlayer(tag) {
+export default function getPlayer(tag, myself = false) {
   const player = k.add([
     sprite("bean"),
     pos(center()),
@@ -14,35 +14,39 @@ export default function getPlayer(tag) {
     tag,
   ]);
 
-  player.collides("food", (food) => {
-    destroy(food);
-    player.biggify(0.5);
-  });
-
-  player.collides("enemy", (e) => {
-    destroy(e);
-    destroy(player);
-    shake(120);
-    // play("explode");
-    // music.detune(-1200);
-    addExplode(center(), 12, 120, 30);
-    wait(1, () => {
-      // music.stop();
-      go("end");
+  if (myself) {
+    player.collides("food", (food) => {
+      destroy(food);
+      player.biggify(0.5);
     });
-  });
 
-  player.collides("missile", (m) => {
-    destroy(m);
-    destroy(player);
-    shake(120);
-    // play("explode");
-    // music.detune(-1200);
-    wait(1, () => {
-      // music.stop();
-      go("end");
+    player.collides("enemy", (e) => {
+      destroy(e);
+      destroy(player);
+      shake(120);
+      // play("explode");
+      // music.detune(-1200);
+      addExplode(center(), 12, 120, 30);
+      wait(1, () => {
+        // music.stop();
+        go("end");
+      });
     });
-  });
+
+    player.collides("missile", (m) => {
+      destroy(m);
+      destroy(player);
+      shake(120);
+      // play("explode");
+      // music.detune(-1200);
+      wait(1, () => {
+        // music.stop();
+        go("end");
+      });
+    });
+
+
+  }
 
   return player;
 }
